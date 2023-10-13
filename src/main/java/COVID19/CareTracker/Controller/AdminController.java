bd4d007a2e149598ece31dbe64c1f3b2d34abbcb
KingNelx <tapiajonel606@gmail.com>
2023-10-13 14:06:01 +0800

    admin service and implementation finish

--- src/main/java/COVID19/CareTracker/Controller/AdminController.java
+++ src/main/java/COVID19/CareTracker/Controller/AdminController.java
@@ -2,12 +2,16 @@ package COVID19.CareTracker.Controller;
 
 import COVID19.CareTracker.Entity.Admin;
 import COVID19.CareTracker.Service.AdminService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/admin")
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminController {
 
     @Autowired
