ee4c8d439f1faaa0badb06b5cee5fa5d4309e475
KingNelx <tapiajonel606@gmail.com>
2023-11-15 18:03:19 +0800

    create services for doctor entity

---
+++ src/main/java/COVID19/CareTracker/Service/DoctorService.java
@@ -0,0 +1,17 @@
+package COVID19.CareTracker.Service;
+
+import COVID19.CareTracker.Entity.Doctor;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+
+@Service
+public interface DoctorService {
+
+    ResponseEntity <String> createDoctor(Doctor doctor);
+    List <Doctor> queryDoctors();
+    Optional <Doctor> queryDoctorByID(String id);
+    ResponseEntity <String> updateDoctorsData(String id, Doctor doctor);
+    ResponseEntity <String> removeDoctorsData(String id;
+}
