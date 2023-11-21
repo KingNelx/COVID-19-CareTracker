85713d8bb70ce3a0f57883c05ce73587f99a7c95
KingNelx <tapiajonel606@gmail.com>
2023-11-21 22:34:48 +0800

    POTANGINA

--- src/main/java/COVID19/CareTracker/Controller/AdminController.java
+++ src/main/java/COVID19/CareTracker/Controller/AdminController.java
@@ -1,11 +1,14 @@
 package COVID19.CareTracker.Controller;
 
 import COVID19.CareTracker.Entity.Admin;
+import COVID19.CareTracker.Repository.DocRepo;
+import COVID19.CareTracker.Repository.PatientRepo;
 import COVID19.CareTracker.Service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +22,10 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+    @Autowired
+    private DocRepo docRepo;
+    @Autowired
+    private PatientRepo patientRepo;
 
     @PostMapping("/create-account")
     public ResponseEntity <String> createAccount(@RequestBody Admin admin){
@@ -36,5 +43,9 @@ public class AdminController {
     }
 
     // PATIENTS
+    @PutMapping("/patient/new-patient")
+    public ResponseEntity <String> newPatient(@RequestBody){
+        
+    }
 
 }
---
+++ src/main/java/COVID19/CareTracker/Repository/PatientRepo.java
@@ -0,0 +1,9 @@
+package COVID19.CareTracker.Repository;
+
+import COVID19.CareTracker.Entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepo extends JpaRepository <Patient, Long> {
+}
